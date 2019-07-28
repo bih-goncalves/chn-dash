84b877a0a381ab9753ff8c635a3b39df32592a94
bih_goncalves <bih_goncalves@hotmail.com>
2019-07-28 03:51:51 -0300

    Starting login authenticantion

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
+import { isAuthenticated } from "./services/auth";
 import Main from './pages/Main';
 import Dash from './pages/Dash';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Login from './pages/SignIn/Login';
+import Register from './pages/SignUp/Register';
 
-function Routes() {
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+        )
+      }
+    />
+  );
+  
+
+const Routes = () => {
     return(
+        <BrowserRouter>
         <Switch>
             <Route path="/" exact component={Main} />
             <Route path="/dash" component={Dash} />
             <Route path="/register" component={Register} />
             <Route path="/login" component={Login} />
         </Switch>
+        </BrowserRouter>
     );
 }
 
