4884fed2530e89f16ecb5d4678bd3b00e82f3106
bih_goncalves <bih_goncalves@hotmail.com>
2019-07-28 02:59:14 -0300

    Changing new collection form's appearance

--- src/pages/Dash.js
+++ src/pages/Dash.js
@@ -2,22 +2,23 @@ import React, { Component } from 'react';
 import api from '../services/api';
 
 import './Dash.css';
-import Modal from './../components/Dialog';
+import NCollect from './NCollect';
 
 import logo from '../assets/light/logo.svg';
 import twitter from '../assets/twitter.svg';
 import media from '../assets/satellite.svg';
 
 class Dash extends Component {
-
     state = {
-        feed : [],
-    };
+        feed: [],
+        narrative: 'narrativa1',
+    }
 
     async componentDidMount() {
         const response = await api.get('collections/?format=json');
 
         this.setState({feed: response.data});
+        //pegar a narrativa dessa request
     }
 
     render() {
@@ -29,7 +30,7 @@ class Dash extends Component {
                             <img className="btn" src={logo} alt="Culture Hack Network"/>
                         </div>
                         <div>
-                            <Modal />
+                            <NCollect narrative={this.state.narrative}/>
                         </div>
                         
                     </div>
@@ -38,18 +39,18 @@ class Dash extends Component {
                 <section id="dash-content">
 
                     {this.state.feed.map(collect => (
-                        <article className={collect.is_done === true?'':'shadow'}>
+                        <article className={collect.isDone === true?'':'shadow'} key={collect.query}>
                             <div className="collect-header">
                                 <span><img className="c-icons" src={collect.collection_type === 'twitter'? twitter:media} alt="Type" /></span>
                                 <a ><strong>{collect.query}</strong></a>
                             </div>
                             <div className="collect-info">
                                 <span>{collect.periodStart} - {collect.periodEnd}</span>
-                                <a class="btn-dash">
-                                <span class="glyphicon glyphicon-arrow-down"></span> Download Data
+                                <a className="btn-dash">
+                                <span className="glyphicon glyphicon-arrow-down"></span> Download Data
                                 </a>
                                 <a style={{'pointer-events':'auto'}}>
-                                <span class="glyphicon glyphicon-option-vertical" ></span>
+                                <span className="glyphicon glyphicon-option-vertical" ></span>
                                 </a>
                             </div> 
                         </article>
---
+++ src/pages/NCollect.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import api from '../services/api';
+
+import './NCollect.css'
+import twitter from '../assets/twitter.svg';
+import media from '../assets/satellite.svg';
+import x_button from '../assets/x_button.svg';
+import new_c from '../assets/light/new_col.svg';
+
+class NCollect extends Component {
+
+    state = {
+        show: 'none',
+        query:'',
+        narrative: this.props.narrative,
+        periodStart:'',
+        periodEnd:'',
+        collection_type:'twitter',
+        isDone:false
+    }
+
+    showleft = () => {
+        this.setState({show : 'block'})
+    }
+
+    esconder = () => {
+        this.setState({show : 'none'})
+    }
+
+    handleSubmit() {
+
+        alert(this.state);
+      }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.showleft}  className="btn">
+                <img src={new_c} alt="Create new collection"/>
+                </button>
+
+                <div className="collect-form" style={{'display':this.state.show}}>
+
+                    <div className="menu-content">
+                        <div className="menu-title">
+                            Create a Collection
+                        </div>
+                        <button onClick={this.esconder} className="close">
+                        <img src={x_button}  alt="Close"/></button>
+
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="new-collect">
+                        <p>Collection Source* <span className="opaco" >(See data from Twitter or News Media)</span>
+                        <br></br>
+
+                        <div class="switch-field">
+                            <input type="radio" id="radio-one" name="collection_type" value="twitter"
+                            checked={this.state.collection_type === 'twitter'} onChange={e => this.setState({ collection_type: e.target.value })} />
+                            <label for="radio-one"><img src={twitter} alt="Type" /> Twitter</label>
+                            <input type="radio" id="radio-two" name="collection_type" value="media" 
+                            checked={this.state.collection_type === 'media'} onChange={e => this.setState({ collection_type: e.target.value })} />
+                            <label for="radio-two"><img src={media} alt="Type" /> Media</label>
+                        </div>
+                        </p>
+                        <p>
+                            Date range* <span className="opaco" >(max 31 Days)</span>
+                            <br></br>
+                            <input type="text" name="periodStart" className="form-text" 
+                            placeholder="Start date" style={{'width':'48%'}}
+                            onChange={e => this.setState({ periodStart: e.target.value })} />
+                            <input type="text" name="periodEnd" className="form-text" 
+                            placeholder="End date" style={{'width':'48%'}}
+                            onChange={e => this.setState({ periodEnd: e.target.value })} />
+                        </p>
+
+                        <p>
+                            Keyword or phrase* <span className="opaco" >(max 3)</span>
+                            <br></br>
+                            <input type="text" name="query" className="form-text" 
+                            placeholder="Add keyword or phrase..." style={{'width':'98%'}}
+                            onChange={e => this.setState({ query: e.target.value })} />
+                        </p>
+                        </div>
+                        <button type="submit" className="menu-bottom" >Create Collection</button>
+                    </form>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default NCollect;
+
+
+
