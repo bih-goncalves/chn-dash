03036e06c72bfd1fac9e9ac390a50dc0a31717b5
bih_goncalves <bih_goncalves@hotmail.com>
2019-08-03 19:43:47 -0300

    Connect dash and new collect

--- src/pages/Dash.js
+++ src/pages/Dash.js
@@ -1,65 +1,58 @@
-import React, { Component } from 'react';
-import api from '../services/api';
+import React from 'react';
+import { connect } from 'react-redux';
 
 import './Dash.css';
 import NCollect from './NCollect';
+//import SelectBox from '../components/Select';
 
 import logo from '../assets/light/logo.svg';
 import twitter from '../assets/twitter.svg';
 import media from '../assets/satellite.svg';
 
-class Dash extends Component {
-    state = {
-        feed: [],
-        narrative: 2,
-    }
-
-    async componentDidMount() {
-        const response = await api.get('collections/?format=json');
-
-        this.setState({feed: response.data});
-        //pegar a narrativa dessa request
-    }
-
-    render() {
-        return (
-            <div className="dash">
-                <header id="dash-header">
-                    <div className="header-content">
-                        <div>
-                            <img className="btn" src={logo} alt="Culture Hack Network"/>
-                        </div>
-                        <div>
-                            <NCollect narrative={this.state.narrative}/>
-                        </div>
-                        
-                    </div>
-                </header>
-
-                <section id="dash-content">
-
-                    {this.state.feed.map(collect => (
-                        <article className={collect.isDone === true?'':'shadow'} key={collect.query}>
-                            <div className="collect-header">
-                                <span><img className="c-icons" src={collect.collection_type === 'twitter'? twitter:media} alt="Type" /></span>
-                                <a ><strong>{collect.query}</strong></a>
-                            </div>
-                            <div className="collect-info">
-                                <span>{collect.periodStart} - {collect.periodEnd}</span>
-                                <a className="btn-dash">
-                                <span className="glyphicon glyphicon-arrow-down"></span> Download Data
-                                </a>
-                                <a style={{pointerEvents:'auto'}}>
-                                <span className="glyphicon glyphicon-option-vertical" ></span>
-                                </a>
-                            </div> 
-                        </article>
-
-                    ))}
-                </section>
-            </div>
-        );
+function novaColeta (form) {
+    return {
+        type: 'NEW_COLLECT',
+        form
     }
 }
 
-export default Dash;
+const Dash = ({ feed , user, dispatch}) => (
+    <div className="dash">
+        <header id="dash-header">
+            <div className="header-content">
+                <div>
+                    <img className="btn" src={logo} alt="Culture Hack Network"/>
+                    </div><div>
+                </div>
+                <div>
+                    <NCollect onSubmit={dispatch}/>
+                </div>
+                
+            </div>
+        </header>
+
+        <section id="dash-content">
+
+            {feed.collections.map(collect => (
+                <article className={collect.isDone === true?'':'shadow'} key={collect.query}>
+                    <div className="collect-header">
+                        <span><img className="c-icons" src={collect.collection_type === 'twitter'? twitter:media} alt="Type" /></span>
+                        <a ><strong>{collect.query}</strong></a>
+                    </div>
+                    <div className="collect-info">
+                        <span>{collect.periodStart} - {collect.periodEnd}</span>
+                        <a className="btn-dash">
+                        <span className="glyphicon glyphicon-arrow-down"></span> Download Data
+                        </a>
+                        <a style={{pointerEvents:'auto'}}>
+                        <span className="glyphicon glyphicon-option-vertical" ></span>
+                        </a>
+                    </div> 
+                </article>
+
+            ))}
+        </section>
+    </div>
+)
+
+export default connect(state => ({ feed: state.feed, user: state.user }))(Dash);
--- src/pages/NCollect.js
+++ src/pages/NCollect.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
 import api from '../services/api';
 
 import './NCollect.css'
@@ -20,41 +19,52 @@ class NCollect extends Component {
         isDone:false
     }
 
-    showleft = () => {
-        this.setState({ show : 'block' });
+    showLeft = () => {
+        if (this.state.show === 'none') {
+            this.setState({show: 'block'})
+        } else {
+            this.setState({show: 'none'})
+        } 
     }
 
-    esconder = () => {
-        this.setState({show : 'none' });
+    changeHandler = (e) => {
+        let name = e.target.name
+        let value = e.target.value
+
+        switch (name) {
+            case 'collection_type':
+                this.setState({collection_type: value})
+                break;
+            case 'periodStart':
+                this.setState({periodStart: value})
+                break;
+            case 'periodEnd':
+                this.setState({periodEnd: value})
+                break;
+            case 'query':
+                this.setState({query: value})
+                break;
+        }
+        
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
         var req = Object.assign({},this.state);
         delete req['show'];
-
-        api.post('collections/',req)
-        .then(function(response){
-            console.log(response);
-            
-          })
-        .catch(function(error) { 
-            console.log(error);
-        }); 
-
-      }
-
-    changeHandler = (e) => {
-        this.setState({
-            [e.target.name]:e.target.value
+        
+        this.showLeft();
+        this.props.onSubmit({
+            type: 'NEW_COLLECT',
+            req
         });
-    }
+      }
 
     render() {
 
         return (
             <div>
-                <button onClick={this.showleft}  className="btn">
+                <button onClick={this.showLeft}  className="btn">
                 <img src={new_c} alt="Create new collection"/>
                 </button>
 
@@ -64,7 +74,7 @@ class NCollect extends Component {
                         <div className="menu-title">
                             Create a Collection
                         </div>
-                        <button onClick={this.esconder} className="close">
+                        <button onClick={this.showLeft} className="close">
                         <img src={x_button}  alt="Close"/></button>
 
                     <form onSubmit={this.handleSubmit}>
@@ -110,6 +120,3 @@ class NCollect extends Component {
 }
 
 export default NCollect;
-
-
-
